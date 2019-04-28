292c1f3f403af9931e9109a72e5aee1b356ab496
libo <1293325947@qq.com>
2019-04-28 20:09:27 +0800

    项目初始化和antd按需加载的配置

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+// 重写webpack配置的文件
+const { override, fixBabelImports } = require('customize-cra');
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css',
+  }),
+);
